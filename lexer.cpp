532f0660a4ab756bacc818265e30fa3e7ff32ecc
Shawn Zhang <yuxiangyxz@gmail.com>
2023-10-14 17:36:45 -0700

    changes

---
+++ lexer.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <vector>
+
+#include "lexer.h"
+#include "token.h"
+
+Lexer::Lexer(const std::string& input) : input_stream(input), current_position(0), current_line(1), current_column(1) {
+}
+
+Token* Lexer::get_next_token() {
+    while (current_position < input_stream.length()) {
+        char current_char = input_stream[current_position];
+
+        if (current_char == '(') {
+            Token* token = create_token(TokenType::LPAREN, current_line, current_column, "(");
+            current_position++;
+            current_column++;
+            return token;
+        }
+        else if (current_char == ')') {
+            Token* token = create_token(TokenType::RPAREN, current_line, current_column, ")");
+            current_position++;
+            current_column++;
+            return token;
+        }
+        else if (current_char == '+') {
+            Token* token = create_token(TokenType::OPERATOR, current_line, current_column, "+");
+            current_position++;
+            current_column++;
+            return token;
+        }
+        else if (current_char == '-') {
+            Token* token = create_token(TokenType::OPERATOR, current_line, current_column, "-");
+            current_position++;
+            current_column++;
+            return token;
+        }
+        else if (current_char == '*') {
+            Token* token = create_token(TokenType::OPERATOR, current_line, current_column, "*");
+            current_position++;
+            current_column++;
+            return token;
+        }
+        else if (current_char == '/') {
+            Token* token = create_token(TokenType::OPERATOR, current_line, current_column, "/");
+            current_position++;
+            current_column++;
+            return token;
+        }
+        else if (std::isdigit(current_char) || (current_char == '.' && std::isdigit(input_stream[current_position + 1]))) {
+            // Recognize numbers
+            int start = current_position;
+            while (current_position < input_stream.length() &&
+                (std::isdigit(input_stream[current_position]) || input_stream[current_position] == '.'))
+            {
+                current_position++;
+                current_column++;
+            }
+            std::string number_text = input_stream.substr(start, current_position - start);
+            Token* token = create_token(TokenType::NUMBER, current_line, current_column - number_text.length() + 1, number_text);
+            return token;
+        }
+        else if (std::isspace(current_char)) {
+            if (current_char == '\n') {
+                current_line++;
+                current_column = 1;
+            }
+            else {
+                current_column++;
+            }
+            current_position++;
+        }
+        else {
+            std::cerr << "Syntax error on line " << current_line << " column " << current_column << "." << std::endl;
+            exit(1);
+        }
+    }
+
+    return create_token(TokenType::END, current_line, current_column, "END");
+}
+
+int main() {
+    std::string input;
+    std::string line;
+    
+    while (std::getline(std::cin, line)) {
+        input += line + '\n';
+    }
+
+    Lexer lexer(input);
+    Token* token;
+
+    do {
+        token = lexer.get_next_token();
+        std::cout << std::right << std::setw(4) << token->line << std::setw(6) << token->column << "  " << token->text << std::endl;
+        free_token(token);
+    } while (token->type != TokenType::END);
+
+    return 0;
+}
---
+++ parser.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+#include "parser.h"
+#include "lexer.h"
+#include "token.h"
+
+// Define an abstract syntax tree (AST) node
+class AstNode::AstNode {
+public:
+    TokenType type;
+    std::string text;
+    AstNode* left;
+    AstNode* right;
+
+    AstNode(TokenType t, const std::string& txt) : type(t), text(txt), left(nullptr), right(nullptr) {}
+};
+
+// Function to parse an S-expression
+AstNode* AstNode::parseSExpression(Lexer& lexer);
+
+// Function to parse an expression
+AstNode* AstNode::parseExpression(Lexer& lexer);
+
+// Function to evaluate an AST node
+double AstNode::evaluate(AstNode* node);
+
+// Function to print an AST in infix form
+void AstNode::printInfix(AstNode* node, bool printParentheses = false);
+
+int main() {
+    std::string input;
+    std::string line;
+
+    // Read input from standard input
+    while (std::getline(std::cin, line)) {
+        input += line + '\n';
+    }
+
+    Lexer lexer(input);
+    Token* token;
+
+    // Initialize a vector to store tokens
+    std::vector<Token*> tokens;
+
+    // Tokenize the input
+    do {
+        token = lexer.get_next_token();
+        if (token->type != TokenType::END) {
+            tokens.push_back(token);
+        } else {
+            free_token(token);
+        }
+    } while (token->type != TokenType::END);
+
+    // Create a parser with the token vector
+    Parser parser(tokens);
+
+    // Parse the S-expression
+    AstNode* root = parseSExpression(parser);
+
+    if (root == nullptr) {
+        // Handle parse errors
+        std::cerr << "Unexpected token at line " << parser.tokens[parser.current_token_index]->line
+                  << " column " << parser.tokens[parser.current_token_index]->column << ": "
+                  << parser.tokens[parser.current_token_index]->text << std::endl;
+        exit(2);
+    }
+
+    // Evaluate the AST
+    double result = evaluate(root);
+
+    // Print the AST in infix form
+    printInfix(root, true);
+
+    // Print the result
+    std::cout << result << std::endl;
+
+    return 0;
+}
+
+// Function to parse an S-expression
+AstNode* AstNode::parseSExpression(Parser& parser) {
+    // A top-level S-expression should contain exactly one expression
+    AstNode* expr = parseExpression(parser);
+
+    // Check if there are extra tokens after the S-expression
+    if (parser.tokens[parser.current_token_index]->type != TokenType::END) {
+        return nullptr; // Parse error
+    }
+
+    return expr;
+}
+
+// Function to parse an expression
+AstNode* AstNode::parseExpression(Parser& parser) {
+    Token* currentToken = parser.tokens[parser.current_token_index];
+
+    if (currentToken->type == TokenType::LPAREN) {
+        parser.current_token_index++; // Consume the opening parenthesis
+        AstNode* leftExpr = parseExpression(parser);
+
+        if (leftExpr == nullptr) {
+            return nullptr; // Parse error
+        }
+
+        currentToken = parser.tokens[parser.current_token_index];
+
+        if (currentToken->type == TokenType::OPERATOR) {
+            parser.current_token_index++; // Consume the operator
+            AstNode* rightExpr = parseExpression(parser);
+
+            if (rightExpr == nullptr) {
+                return nullptr; // Parse error
+            }
+
+            currentToken = parser.tokens[parser.current_token_index];
+            if (currentToken->type == TokenType::RPAREN) {
+                parser.current_token_index++; // Consume the closing parenthesis
+                AstNode* rootNode = new AstNode(TokenType::OPERATOR, currentToken->text);
+                rootNode->left = leftExpr;
+                rootNode->right = rightExpr;
+                return rootNode;
+            } else {
+                return nullptr; // Parse error
+            }
+        } else if (currentToken->type == TokenType::RPAREN) {
+            parser.current_token_index++; // Consume the closing parenthesis
+            return leftExpr; // Return the expression within parentheses
+        } else {
+            return nullptr; // Parse error
+        }
+    } else if (currentToken->type == TokenType::NUMBER) {
+        parser.current_token_index++; // Consume the number
+        return new AstNode(TokenType::NUMBER, currentToken->text);
+    } else {
+        return nullptr; // Parse error
+    }
+}
+
+// Function to evaluate an AST node
+double AstNode::evaluate(AstNode* node) {
+    if (node->type == TokenType::NUMBER) {
+        return std::stod(node->text);
+    } else {
+        double leftValue = evaluate(node->left);
+        double rightValue = evaluate(node->right);
+        if (node->text == "+") {
+            return leftValue + rightValue;
+        } else if (node->text == "-") {
+            return leftValue - rightValue;
+        } else if (node->text == "*") {
+            return leftValue * rightValue;
+        } else if (node->text == "/") {
+            if (rightValue != 0) {
+                return leftValue / rightValue;
+            } else {
+                std::cerr << "Runtime error: division by zero." << std::endl;
+                exit(3);
+            }
+        }
+    }
+    return 0.0;
+}
+
+// Function to print an AST in infix form
+void AstNode::printInfix(AstNode* node, bool printParentheses) {
+    if (node->type == TokenType::NUMBER) {
+        std::cout << node->text;
+    } else {
+        if (printParentheses) {
+            std::cout << "(";
+        }
+        printInfix(node->left, true);
+        std::cout << " " << node->text << " ";
+        printInfix(node->right, true);
+        if (printParentheses) {
+            std::cout << ")";
+        }
+    }
+}
