c7b31eec9055f1e2d4df88ffba6b67cff9f3b555
Yiguang <yzhu621@ucsb.edu>
2023-10-16 15:16:51 -0700

    s

--- src/lex.cpp
+++ src/lex.cpp
@@ -4,25 +4,96 @@
 #include <vector>
 
 #include "lib/lex.h"
-#include "lib/token.h"
-#include "lex.h"
+
 
 Lexer::Lexer(std::istream& input) : input(input), syntaxError(false) {
     // Constructor implementation
-    // Initialize other member variables here if needed
 }
 
 void Lexer::readNextToken() {
-    // Implement token recognition logic
+    char currentChar;
+    input.get(currentChar);
+
+    // Skip whitespace characters
+    while (std::isspace(currentChar)) {
+        if (currentChar == '\n') {
+            // Update line and reset column when a newline is encountered
+            ++tokens.back().line;
+            tokens.back().column = 1;
+        } else {
+            ++tokens.back().column;
+        }
+        input.get(currentChar);
+    }
+
+    if (input.eof()) {
+        // Reached the end of input
+        tokens.push_back(Token(TokenType::END, tokens.back().line, tokens.back().column + 1));
+        return;
+    }
+
+    if (currentChar == '(') {
+        tokens.push_back(Token(TokenType::LEFT_PAREN, tokens.back().line, tokens.back().column));
+    } else if (currentChar == ')') {
+        tokens.push_back(Token(TokenType::RIGHT_PAREN, tokens.back().line, tokens.back().column));
+    } else if (currentChar == '+') {
+        tokens.push_back(Token(TokenType::ADD, tokens.back().line, tokens.back().column));
+    } else if (currentChar == '-') {
+        tokens.push_back(Token(TokenType::SUBTRACT, tokens.back().line, tokens.back().column));
+    } else if (currentChar == '*') {
+        tokens.push_back(Token(TokenType::MULTIPLY, tokens.back().line, tokens.back().column));
+    } else if (currentChar == '/') {
+        tokens.push_back(Token(TokenType::DIVIDE, tokens.back().line, tokens.back().column));
+    } else if (std::isdigit(currentChar) || currentChar == '.') {
+        // Read a simple floating point number
+        std::string number;
+        number += currentChar;
+        while (input.get(currentChar)) {
+            if (std::isdigit(currentChar) || currentChar == '.') {
+                number += currentChar;
+            } else {
+                input.unget();
+                break;
+            }
+        }
+        tokens.push_back(Token(std::stod(number), tokens.back().line, tokens.back().column));
+    } else {
+        // Invalid character, report syntax error
+        reportSyntaxError("Invalid character: " + currentChar);
+    }
+
+    ++tokens.back().column;
 }
 
 void Lexer::skipWhitespace() {
-    // Implement whitespace skipping logic
+    // Skip whitespace characters
+    char currentChar;
+    while (input.get(currentChar)) {
+        if (std::isspace(currentChar)) {
+            if (currentChar == '\n') {
+                // Update line and reset column when a newline is encountered
+                ++tokens.back().line;
+                tokens.back().column = 1;
+            } else {
+                ++tokens.back().column;
+            }
+        } else {
+            input.unget();
+            break;
+        }
+    }
 }
 
 std::vector<Token> Lexer::tokenize() {
-    // Implement the overall tokenization process
-    // Call readNextToken in a loop until END token is reached
+    while (true) {
+        skipWhitespace();
+        readNextToken();
+
+        if (tokens.back().type == TokenType::END) {
+            break;
+        }
+    }
+
     return tokens;
 }
 
