7fd8ed2956f9986635736074b65965864e5b30e9
Yiguang <yzhu621@ucsb.edu>
2023-10-16 15:22:16 -0700

    s

--- src/lex.cpp
+++ src/lex.cpp
@@ -5,7 +5,27 @@
 
 #include "lib/lex.h"
 
+int main() {
+    // Create an input stream (you can replace this with your input source)
+    std::istringstream input("(+(-2 4.444 )\n32(* 5 13.45)(");
 
+    // Create a Lexer object
+    Lexer lexer(input);
+
+    // Tokenize the input
+    std::vector<Token> tokens = lexer.tokenize();
+
+    // Print tokens and positions
+    lexer.printTokensAndPositions();
+
+    // Check for syntax errors
+    if (lexer.hasSyntaxError()) {
+        std::cerr << "Syntax error detected." << std::endl;
+        return 1; // Exit with an error code
+    }
+
+    return 0; // Exit with a success code
+}
 Lexer::Lexer(std::istream& input) : input(input), syntaxError(false) {
     // Constructor implementation
 }
