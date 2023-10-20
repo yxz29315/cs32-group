47ae10f8992f9dc597f1bd66efd08bb71c17c309
Yiguang <yzhu621@ucsb.edu>
2023-10-19 19:09:27 -0700

    s

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
-#include <cctype>
-#include <string>
-#include <vector>
-#include <sstream> // Include this header for std::istringstream
-
-#include "lib/Lexer.h"
-
+#include "lexer.h"
 
 int main() {
-    // Create an input stream (you can replace this with your input source)
-    std::istringstream input("(+(-2 4.444 )\n32(* 5 13.45)(");
-
-    // Create a Lexer object
-    Lexer lexer(input);
-
-    // Tokenize the input
-    std::vector<Token> tokens = lexer.tokenize();
-
-    // Print tokens and positions
-    lexer.printTokensAndPositions();
-
-    // Check for syntax errors
-    if (lexer.hasSyntaxError()) {
-        std::cerr << "Syntax error detected." << std::endl;
-        return 1; // Exit with an error code
+    // Read input from standard input
+    std::string input;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        input += line;
+        input += '\n';  // Add newline for line separation
     }
 
-    return 0; // Exit with a success code
+    // Create a lexer with the input
+    Lexer lexer(input);
+
+    // Print the tokens
+    lexer.printTokens();
+
+    return 0;
 }
+
