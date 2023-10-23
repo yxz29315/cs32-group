dad8f689f1d0098c4a6ba0e5d7cc442d812f402b
Yiguang <yzhu621@ucsb.edu>
2023-10-23 15:29:12 -0700

    s

--- src/lib/Lexer.cpp
+++ src/lib/Lexer.cpp
@@ -131,6 +131,6 @@ void Lexer::readTokens() {
 void Lexer::printTokens() {
     
     for (Token t : tokens) {
-        std::cout << std::right << std::setw(4) << t.line << std::setw(4)  << t.column << t.text << std::endl;
+        std::cout << std::right << std::setw(4) << t.line << std::setw(4)  << t.column << std::setw(4) << t.text << std::endl;
     }
 }
