e7fd1fd2ba8fcc1c7960aedff0cd182bfb535f4e
Shawn Zhang <yuxiangyxz@gmail.com>
2023-10-23 21:46:01 -0700

    m

--- src/lib/Lexer.cpp
+++ src/lib/Lexer.cpp
@@ -103,19 +103,21 @@ void Lexer::readTokens() {
         } else if (currentChar == ' ') {
             nextCol++;
         }
+        else if (input.eof()) {
+            // Add END token
+            Token end;
+            end.line = nextLine;
+            end.column = nextCol;
+            end.type = Token::TokenType::END;
+            end.text = "END";
+            tokens.push_back(end);
+        }
         else {
             // Invalid character
             std::cerr << "Syntax error on line " << nextLine << " column " << nextCol << ".\n";
             exit(1);
         }
     }
-    // Add END token
-    Token end;
-    end.line = nextLine;
-    end.column = nextCol;
-    end.type = Token::TokenType::END;
-    end.text = "END";
-    tokens.push_back(end);
 }
 
 
