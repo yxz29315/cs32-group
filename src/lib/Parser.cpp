fe2d6499a25cce87f7cdfb50fb37f92894b2759a
ryanvo504 <ryanvo@ucsb.edu>
2023-11-06 13:39:30 -0800

    hm

--- src/lib/Parser.cpp
+++ src/lib/Parser.cpp
@@ -118,7 +118,6 @@ AstNode *Parser::ops(deque<Token> &x)
     AstNode *temp = nullptr;
     Num *temp2 = nullptr;
     NodeKey* temp3 = nullptr;
-    Op* temp4 = nullptr;
     x.pop_front();
     int counter = 0; // count how many kids there are, throw error if 0
 
@@ -138,8 +137,8 @@ AstNode *Parser::ops(deque<Token> &x)
         }
         else if (x.front().type == Token::TokenType::OPERATOR)
         {
-            temp4 = new Op(x.front().text[0]);
-            root->addNode(temp4);
+            temp = ops(x);
+            root->addNode(temp);
             x.pop_front();
         }
         else 
