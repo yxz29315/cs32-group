fa0502867a6ca586390db74264350a9dd2694076
ryanvo504 <ryanvo@ucsb.edu>
2023-11-06 13:51:46 -0800

    no op in op

--- src/lib/Parser.cpp
+++ src/lib/Parser.cpp
@@ -121,7 +121,7 @@ AstNode *Parser::ops(deque<Token> &x)
     x.pop_front();
     int counter = 0; // count how many kids there are, throw error if 0
 
-    while (x.front().type == Token::TokenType::LEFTP || x.front().type == Token::TokenType::NUMBER || x.front().type == Token::TokenType::IDENTIFIER || x.front().type == Token::TokenType::OPERATOR)
+    while (x.front().type == Token::TokenType::LEFTP || x.front().type == Token::TokenType::NUMBER || x.front().type == Token::TokenType::IDENTIFIER )
     {
         if (x.front().type == Token::TokenType::LEFTP)
         {
